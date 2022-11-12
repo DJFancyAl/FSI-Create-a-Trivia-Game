8a3afd2f94b291ea5a004026a2893b5577037f3a
DJFancyAl <abluemleiv@gmail.com>
2022-11-11 19:21:08 -0500

    Makes the Trivia Game Work

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your Code Here
+let userName = window.prompt('Please enter your name:')
+let userScore = 0
+let playGame = true
+
+const runGame = () => {
+    for(let i = 0; i < questions.length; i++){
+        let question = questions[i]
+        let userAnswer = window.prompt(question.text)
+    
+        if (userAnswer.toUpperCase() === question.correctAnswer) {
+            userScore += 10
+        }
+    }
+    window.alert(`Congratulations ${userName}. Your score is: ${userScore}% `)
+}
+
+while (playGame) {
+    runGame()
+    let playAgain = window.prompt(`Okay ${userName}. Would you like to play again? (Yes or No)`)
+    if(playAgain.toUpperCase() == 'YES'){
+        userScore = 0
+        playGame = true
+    } else {
+        playGame = false
+    }
+}
+
+window.alert('Game finished!')
